b5ccbd6556bf676e8f406d91e6b623bb10c97af6
facostamit <56843815+facostamit@users.noreply.github.com>
2022-08-16 22:17:59 -0700

    adding neural_metric and notebook for computing curvature

--- abn/neural_metric.py
+++ abn/neural_metric.py
@@ -5,9 +5,6 @@ import geomstats.backend as gs
 
 from geomstats.geometry.pullback_metric import PullbackMetric
 import torch
-import numpy
-import matplotlib.pyplot as plt
-from geomstats.geometry.riemannian_metric import RiemannianMetric
 
 
 class NeuralMetric(PullbackMetric):
@@ -21,16 +18,53 @@ class NeuralMetric(PullbackMetric):
     def injectivity_radius(self, base_point):
         return gs.pi
 
-    # def immersion_i(self, i, x):
-    #     return self.immersion(x)[i]
-
-
     def mean_curvature(self, base_point):
 
         H = gs.zeros((self.embedding_dim,))
         for i in range(self.embedding_dim):
-            H[i] = torch.autograd.functional.hessian(func = lambda x: self.immersion(x)[i],inputs = base_point)  #self.f_i[self.immersion,x,i])
+            H[i] = torch.autograd.functional.hessian(
+                func = lambda x: self.immersion(x)[i],
+                inputs = base_point)
         return H
+
+
+
+
+def get_neural_immersion(model):
+    """ NEED TO FIX DOCUMENTATION HERE
+    
+    
+    Compute the immersion from S^1 [position of rat along circle] to R^N. 
+    
+    R^N represents the place cell neural state space.
+    This function f is the composition of:
+        a function p : S^1 -> R^2   {takes an angle, returns (x,y) coords}
+        and a function Dec : R^2 -> R^N     {takes (x,y) coords in latent space, returns reconstruction}
+
+        f(theta) = Dec(p(theta))
+
+    Parameters
+    ----------
+    
+    model : models.fc_vae.VAE
+        VAE model. The decoder that reconstructs neural activity is used to define the immersion.
+    theta : array_like, shape=[batch_size,]
+        Position of rat along circular track, expressed as an angle. 
+
+    Returns
+    -------
+    x_mu : array_like, shape=[batch_size, data_dim]
+        Mean of generative model likelihood distribution. [CURRENTLY ASSUMED TO BE GAUSSIAN]
+    """
+
+    def neural_immersion(theta):
+        z = gs.stack([gs.cos(theta),gs.sin(theta)],axis=-1)
+        recon_x = model.decode(z)
+        x_mu, _ = recon_x
+        return x_mu
+    
+    
+    return neural_immersion
         
 
 
