4ac0a5e915631f08ed70e152c6d77e3478afe06f
Nina Miolane <nmiolane@gmail.com>
2022-12-30 18:49:28 -0800

    Add Bayesian HyperOpt + Scheduler Hyperband async

--- neuralgeom/default_config.py
+++ neuralgeom/default_config.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 
 import torch
+from ray.tune.search.hyperopt import HyperOptSearch
 
 os.environ["GEOMSTATS_BACKEND"] = "pytorch"
 
@@ -32,7 +33,6 @@ logging.basicConfig(level=logging.INFO)
 
 # Results
 project = "neuralgeom"
-now = str(datetime.now().replace(second=0, microsecond=0))
 trained_model_path = None
 
 ### Fixed experiment parameters ###
@@ -135,7 +135,7 @@ gen_likelihood_type = "gaussian"
 scheduler = False
 log_interval = 20
 checkpt_interval = 20
-n_epochs = 150  # 240  #
+n_epochs = 80  # 240
 sftbeta = 4.5
 beta = 0.03  # 0.03  # weight for KL term
 gamma = 20  # 20  # weight for latent loss term
@@ -145,17 +145,17 @@ gamma = 20  # 20  # weight for latent loss term
 # Except for lr_min and lr_max which are floats
 lr_min = 0.00001
 lr_max = 0.1
-batch_size = [8, 32]
+batch_size = [32]
 encoder_width = [100]
 encoder_depth = [4]
 decoder_width = [100]
-decoder_depth = [3, 6, 10]
+decoder_depth = [6, 10]
 
 # Number of times to sample from the
 # hyperparameter space. Defaults to 1. If `grid_search` is
 # provided as an argument, the grid will be repeated
 # `num_samples` of times. If this is -1, (virtually) infinite
 # samples are generated until a stopping condition is met.
-num_samples = 5
+num_samples = 2
 # Doc on tune.run:
 # https://docs.ray.io/en/latest/_modules/ray/tune/tune.html
