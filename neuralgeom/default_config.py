ea05fc8cdcceb0e24c4b032c68bd05b2fa7d80d4
Nina Miolane <nmiolane@gmail.com>
2023-01-02 07:27:41 -0800

    Large run with expt 41

--- neuralgeom/default_config.py
+++ neuralgeom/default_config.py
@@ -116,13 +116,15 @@ for one_dataset_name in dataset_name:
         raise ValueError(f"Dataset name {one_dataset_name} not recognized.")
 
 # Ignored if dataset_name != "experimental"
-expt_id = ["34"]  # hd: with head direction
-timestep_microsec = [int(1e6)]
+expt_id = ["41"]  # hd: with head direction
+timestep_microsec = [int(1e6), int(1e5)]
 smooth = [True]
-select_gain_1 = [True]
+# Note: if there is only one gain (gain 1), it will be selected
+# even if select gain 1 is false
+select_gain_1 = [True, False]
 
 # Ignored if dataset_name == "experimental"
-n_times = [10]  # actual number of times is sqrt_ntimes ** 2
+n_times = [1000]  # actual number of times is sqrt_ntimes ** 2
 embedding_dim = [2, 3]
 distortion_amp = [0.4]
 noise_var = [1e-3]
@@ -135,7 +137,7 @@ gen_likelihood_type = "gaussian"
 scheduler = False
 log_interval = 20
 checkpt_interval = 20
-n_epochs = 80  # 240
+n_epochs = 150  # 240
 sftbeta = 4.5
 beta = 0.03  # 0.03  # weight for KL term
 gamma = 30  # 20  # weight for latent loss term
@@ -145,17 +147,20 @@ gamma = 30  # 20  # weight for latent loss term
 # Except for lr_min and lr_max which are floats
 lr_min = 0.00001
 lr_max = 0.1
-batch_size = [32]
+batch_size = [8, 16, 32]
 encoder_width = [100]
 encoder_depth = [4]
-decoder_width = [100, 200, 300]
-decoder_depth = [5, 10, 50, 100]
+decoder_width = [50, 100, 200, 300]
+decoder_depth = [5, 10, 20, 50, 100]
 
 # Number of times to sample from the
 # hyperparameter space. Defaults to 1. If `grid_search` is
 # provided as an argument, the grid will be repeated
 # `num_samples` of times. If this is -1, (virtually) infinite
 # samples are generated until a stopping condition is met.
-num_samples = 20
+# Given that 8/10 gpus can run at the same time,
+# We choose a multiple of 8.
+num_samples = 256
+sweep_metric = "test_loss"
 # Doc on tune.run:
 # https://docs.ray.io/en/latest/_modules/ray/tune/tune.html
